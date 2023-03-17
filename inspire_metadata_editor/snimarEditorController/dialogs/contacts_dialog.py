71ffcd677774867401dfa43a067520db91779169
Alexander Bruy <alexander.bruy@gmail.com>
2023-03-17 19:13:56 +0200

    cleanup contact editor a bit and make some preparations for add new organization functionality

--- inspire_metadata_editor/snimarEditorController/dialogs/contacts_dialog.py
+++ inspire_metadata_editor/snimarEditorController/dialogs/contacts_dialog.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 ##############################################################################
 #
 #  Title:   snimarEditorController/dialogs/contacts_dialog.py
@@ -24,59 +25,61 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from builtins import str
-from builtins import range
-from qgis.PyQt import QtCore as qcore
-from qgis.PyQt import QtGui as qgui
-from qgis.PyQt import QtWidgets as qwidgets
-from qgis.PyQt.QtCore import QMetaObject, Qt
-from qgis.PyQt.QtWidgets import QWidget, QToolTip, QDateTimeEdit, QDialog
-from qgis.PyQt.QtGui import QCursor, QFont
-
-from EditorMetadadosMarswInforbiomares.snimarQtInterfaceView.pyuic4GeneratedSourceFiles.dialogs import contactListManagerWindow
-from EditorMetadadosMarswInforbiomares.snimarEditorController.models import table_list_aux as tla
-from EditorMetadadosMarswInforbiomares.snimarEditorController.models import customComboBoxModel as cus
-
-import platform
 
 import os
 import json
-import qgis
+import platform
+
+from qgis.PyQt import uic
+from qgis.PyQt.QtCore import QMetaObject, Qt, QRegExp
+from qgis.PyQt.QtWidgets import QWidget, QToolTip, QDateTimeEdit, QDialog, QPushButton, QComboBox, QMessageBox, QListWidget, QLineEdit, QListWidgetItem
+from qgis.PyQt.QtGui import QCursor, QFont, QIcon
+
+from EditorMetadadosMarswInforbiomares.snimarEditorController.models import table_list_aux as tla
+from EditorMetadadosMarswInforbiomares.snimarEditorController.models import customComboBoxModel as cus
+
+from EditorMetadadosMarswInforbiomares.snimarQtInterfaceView.pyuic4GeneratedSourceFiles.dialogs import contactListManagerWindow
+
+from inspire_metadata_editor.constants import PLUGIN_ROOT
+
+WIDGET, BASE = uic.loadUiType(os.path.join(PLUGIN_ROOT, "ui", "contacts_dialog.ui"))
 
 CONTACTFILE = os.path.join(os.path.join(os.path.abspath(os.path.expanduser('~')), '.snimar'), 'contact_list.json')
 OUTRA = "Outra - Especificar Abaixo"
 
 
-class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
+class ContactsDialog(WIDGET, BASE):
     def __init__(self, parent, edition_mode=True):
-        super(ContactsDialog, self).__init__(parent)
+        super().__init__(parent)
+        self.setupUi(self)
+
         if platform.system() != "Linux":
             font = QFont()
             font.setFamily(u"Segoe UI Symbol")
             self.setFont(font)
+
         # VIEW SETUP#############################################################
-        self.setupUi(self)
         self.superParent = self.parent()
 
-        for btn in self.findChildren(qwidgets.QPushButton, qcore.QRegExp('btn_*')):
+        for btn in self.findChildren(QPushButton, QRegExp('btn_*')):
             if '_add_' in btn.objectName():
-                btn.setIcon(qgui.QIcon(':/resourcesFolder/icons/plus_icon.svg'))
+                btn.setIcon(QIcon(':/resourcesFolder/icons/plus_icon.svg'))
                 btn.setText('')
             elif '_del_' in btn.objectName():
-                btn.setIcon(qgui.QIcon(':/resourcesFolder/icons/delete_icon.svg'))
+                btn.setIcon(QIcon(':/resourcesFolder/icons/delete_icon.svg'))
                 btn.setText('')
             elif '_clear_' in btn.objectName():
-                btn.setIcon(qgui.QIcon(':/resourcesFolder/icons/delete_field.svg'))
+                btn.setIcon(QIcon(':/resourcesFolder/icons/delete_field.svg'))
 
         lists = ['phone', 'fax', 'email']
         for item in lists:
-            btn = self.findChild(qwidgets.QPushButton, 'btn_add_' + item)
+            btn = self.findChild(QPushButton, 'btn_add_' + item)
             btn.clicked.connect(self.add_list_row)
-            btn = self.findChild(qwidgets.QPushButton, 'btn_del_' + item)
+            btn = self.findChild(QPushButton, 'btn_del_' + item)
             btn.clicked.connect(self.del_list_row)
 
-        for info in self.findChildren(qwidgets.QPushButton, qcore.QRegExp('info_*')):
-            info.setIcon(qgui.QIcon(':/resourcesFolder/icons/help_icon.svg'))
+        for info in self.findChildren(QPushButton, QRegExp('info_*')):
+            info.setIcon(QIcon(':/resourcesFolder/icons/help_icon.svg'))
             info.setText('')
             info.pressed.connect(self.printHelp)
 
@@ -130,12 +133,11 @@ class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
             # ACTIONS SETUP
             self.btn_contact_save.clicked.connect(self.save_contact)  # save contact
             self.btn_cancelClose.clicked.connect(self.cancelChanges)  # cancel changes and revert contact
-            self.btn_contact_add.clicked.connect(
-                self.new_contact)  # add new contact if changes in current contact exists ask...
+            self.btn_contact_add.clicked.connect(self.new_contact)  # add new contact if changes in current contact exists ask...
             self.btn_contact_del.clicked.connect(self.delete_contact)  # delete contact with confirmation
+            self.contact_list.itemClicked.connect(self.selection_changed)  # change current selection if changes exists ask...
 
-            self.contact_list.itemClicked.connect(
-                self.selection_changed)  # change current selection if changes exists ask...
+            self.btn_add_organization.clicked.connect(self.add_new_organization)
         else:  # EXPORT MODE
             self.contactPanel.hide()
             self.btn_add_contact_metadata.setVisible(True)
@@ -147,20 +149,18 @@ class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
         self.contact_list.itemClicked.connect(self.selection_changed_export_Mode)  # change current selection
         # ACTIONS FUNCTIONS
         self.eater = tla.EatWheel()
-        for x in self.findChildren(qwidgets.QComboBox):
+        for x in self.findChildren(QComboBox):
             x.installEventFilter(self.eater)
             x.setFocusPolicy(Qt.StrongFocus)
         for x in self.findChildren(QDateTimeEdit):
             x.installEventFilter(self.eater)
             x.setFocusPolicy(Qt.StrongFocus)
 
-    @qcore.pyqtSlot()
     def printHelp(self):
         QToolTip.showText(QCursor.pos(), tla.formatTooltip(
             self.superParent.helps['contactListManagerWindow'][self.sender().objectName()]),
                           None)
 
-    @qcore.pyqtSlot()
     def save_contact(self):
 
         flags = self.mandatory_state()
@@ -199,20 +199,17 @@ class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
             self.btn_cancelClose.setDisabled(True)
             self.btn_contact_save.setDisabled(True)
 
-    @qcore.pyqtSlot()
     def changesMade(self):
         self.changes = True
         self.btn_cancelClose.setDisabled(False)
         self.btn_contact_save.setDisabled(False)
 
-    @qcore.pyqtSlot()
     def cancelChanges(self):
         self.present_contact()
         self.changes = False
         self.btn_cancelClose.setDisabled(True)
         self.btn_contact_save.setDisabled(True)
 
-    @qcore.pyqtSlot()
     def new_contact(self):
         self.changes = True
         self.current_contact = None
@@ -221,17 +218,16 @@ class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
         self.contact_list.clearSelection()
         self.contactPanel.setDisabled(False)
 
-    @qcore.pyqtSlot()
     def delete_contact(self):
-        message = qwidgets.QMessageBox()
+        message = QMessageBox()
         message.setModal(True)
-        message.setWindowTitle(u'Remover contacto?')
-        message.setIcon(qwidgets.QMessageBox.Warning)
-        message.setText(u"Tem a certeza que pretende remover contacto?\n(Operação Irreversivel!)")
-        message.addButton(u'Remover', qwidgets.QMessageBox.AcceptRole)
-        message.addButton(u'Cancelar', qwidgets.QMessageBox.RejectRole)
+        message.setWindowTitle(self.tr('Remover contacto?'))
+        message.setIcon(QMessageBox.Warning)
+        message.setText(self.tr("Tem a certeza que pretende remover contacto?\n(Operação Irreversivel!)"))
+        message.addButton(self.tr('Remover'), QMessageBox.AcceptRole)
+        message.addButton(self.tr('Cancelar'), QMessageBox.RejectRole)
         ret = message.exec_()
-        if ret == qwidgets.QMessageBox.AcceptRole:
+        if ret == QMessageBox.AcceptRole:
             # get current selected index in list
             index = self.contact_list.currentRow()
             if index < 0:
@@ -245,19 +241,18 @@ class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
             self.present_contact()
             self.changes = False
 
-    @qcore.pyqtSlot()
     def selection_changed(self):
         index = self.contact_list.currentRow()
         if self.changes and self.current_contact is not None:
-            message = qwidgets.QMessageBox()
+            message = QMessageBox()
             message.setModal(True)
-            message.setWindowTitle(u'Existem Alterações.')
-            message.setIcon(qwidgets.QMessageBox.Warning)
-            message.setText(u"Deseja guardar as alterações efectuadas?")
-            message.addButton(u'Guardar', qwidgets.QMessageBox.AcceptRole)
-            message.addButton(u'Cancelar', qwidgets.QMessageBox.RejectRole)
+            message.setWindowTitle(self.tr('Existem Alterações.'))
+            message.setIcon(QMessageBox.Warning)
+            message.setText(self.tr("Deseja guardar as alterações efectuadas?"))
+            message.addButton(self.tr('Guardar'), QMessageBox.AcceptRole)
+            message.addButton(self.tr('Cancelar'), QMessageBox.RejectRole)
             ret = message.exec_()
-            if ret == qwidgets.QMessageBox.AcceptRole:
+            if ret == QMessageBox.AcceptRole:
                 self.save_contact()
         self.contactPanel.setDisabled(False)
         self.changes = False
@@ -266,7 +261,6 @@ class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
         self.present_contact()
         self.cancelChanges()
 
-    @qcore.pyqtSlot()
     def selection_changed_export_Mode(self):
         index = self.contact_list.currentRow()
         self.current_contact = self.contact_array[index]
@@ -293,35 +287,32 @@ class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
             items_labels = [item_text[0] + ' - ' + item_text[1] for item_text in list_item_texts]
             self.contact_list.addItems(items_labels)
 
-    @qcore.pyqtSlot()
     def add_list_row(self):
         """Add a new row to the specific list that shares the same name with the button that
         emits the signal that fires the slot."""
         sender_name = self.sender().objectName()
-        target_list = self.findChild(qwidgets.QListWidget, sender_name.split('_')[2])
-        target_input = self.findChild(qwidgets.QLineEdit, sender_name.split('_')[2] + '_input')
+        target_list = self.findChild(QListWidget, sender_name.split('_')[2])
+        target_input = self.findChild(QLineEdit, sender_name.split('_')[2] + '_input')
 
         if target_list is not None and target_input is not None:
             input_text = target_input.text()
             if len(input_text) > 0:
-                item = qwidgets.QListWidgetItem(input_text)
-                item.setFlags(item.flags() | qcore.Qt.ItemIsEditable)
+                item = QListWidgetItem(input_text)
+                item.setFlags(item.flags() | Qt.ItemIsEditable)
                 target_list.addItem(item)
                 target_input.setText('')
 
-    @qcore.pyqtSlot()
     def del_list_row(self):
         """Works like the add_list_row, but deletes the selected row from the list that shares
         its name with button that emits the signal."""
         sender_name = self.sender().objectName()
-        target_list = self.findChild(qwidgets.QListWidget, sender_name.split('_')[2])
+        target_list = self.findChild(QListWidget, sender_name.split('_')[2])
 
         index = target_list.currentRow()
         if index is not None:
             target_list.takeItem(index)
             target_list.setCurrentRow(-1)
 
-    @qcore.pyqtSlot()
     def present_contact(self):
         self.name.clear()
         self.organization.clear()
@@ -378,12 +369,12 @@ class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
     def setup_mandatory_label(self):
         state = self.mandatory_state()
         if not state['organization']:
-            label_text = u'Nome da Organização'
+            label_text = self.tr('Nome da Organização')
             label_text += u' ' + u'\u26a0'
             self.label_organization.setText(label_text)
-            self.label_organization.setToolTip(u'Elemento obrigatório em falta')
+            self.label_organization.setToolTip(self.tr('Elemento obrigatório em falta'))
         else:
-            label_text = u'Nome da Organização'
+            label_text = self.tr('Nome da Organização')
             self.label_organization.setText(label_text)
             self.label_organization.setToolTip(u'')
 
@@ -417,10 +408,12 @@ class ContactsDialog(QDialog, contactListManagerWindow.Ui_contacts_dialog):
             self.organization.setText(self.combo_org.currentText())
 
     def set_org(self, org_name):
-
         if org_name in [x.term for x in list(self.orgs.values())]:
             self.combo_org.setCurrentIndex(self.combo_org.findText(org_name))
         else:
             self.combo_org.setCurrentIndex(0)
             self.check_org()
             self.organization.setText(org_name)
+
+    def add_new_organization(self):
+        pass
